8128bdeec31a010b8289292b42a9a9c7a507d16b
Bartlomiej Mariusz Skorulski <bartekskorulski@gmail.com>
2019-12-27 18:11:31 +0100

    refactor: corrections

--- app.js
+++ app.js
@@ -4,19 +4,19 @@ const os = require('os');
 const port = 80;
 
 var handler = function(request, response) {
+    response.setHeader('Content-Type', 'text/plain');
     if (request.url === '/') {
         console.log(
             "Received request from " + request.connection.remoteAddress
         ); 
         response.statusCode = 200;
-        response.setHeader('Content-Type', 'text/plain');
         text = "Hello World\n" +
         "from " + os.hostname() + "\n";
         response.end(text);
     } else if (request.url === '/healthCheck') {
         var randomNumber = Math.random( );
         console.log(randomNumber);
-        if (randomNumber < 0.25) {
+        if (randomNumber < .5) {
             response.writeHead(500);
             response.end("Not OK");
         } else {
